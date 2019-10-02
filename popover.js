049f3f99ebc06212ecae91785af61869794dbff0
bsninfo <56056312+bsninfo@users.noreply.github.com>
2019-10-02 08:57:28 +0700

    First Commit
    import all html, css and js

---
+++ popover.js
@@ -0,0 +1,30 @@
+$('#popoverData').popover();
+$('#popoverProposal').popover({ trigger: "hover" });
+$('#popoverProposal1').popover({ trigger: "hover" });
+$('#popoverProposal2').popover({ trigger: "hover" });
+$('#popoverProposal3').popover({ trigger: "hover" });
+$('#popoverProposal4').popover({ trigger: "hover" });
+$('#popoverProposal5').popover({ trigger: "hover" });
+
+$('#popoverSubstansiPresentasi').popover({ trigger: "hover" });
+$('#popoverSubstansiPresentasi1').popover({ trigger: "hover" });
+$('#popoverSubstansiPresentasi2').popover({ trigger: "hover" });
+
+$('#popoverProposalKirim').popover({ trigger: "hover" });
+$('#popoverProposalKirim1').popover({ trigger: "hover" });
+$('#popoverProposalKirim2').popover({ trigger: "hover" });
+$('#popoverProposalKirim3').popover({ trigger: "hover" });
+
+
+$('#Tab2popoverData').popover();
+$('#Tab2popoverProposal').popover({ trigger: "hover" });
+$('#Tab2popoverProposal1').popover({ trigger: "hover" });
+$('#Tab2popoverProposal2').popover({ trigger: "hover" });
+$('#Tab2popoverProposal3').popover({ trigger: "hover" });
+$('#Tab2popoverProposal4').popover({ trigger: "hover" });
+$('#Tab2popoverProposal5').popover({ trigger: "hover" });
+
+
+$('#Tab2popoverSubstansiPresentasi').popover({ trigger: "hover" });
+$('#Tab2popoverSubstansiPresentasi1').popover({ trigger: "hover" });
+$('#Tab2popoverSubstansiPresentasi2').popover({ trigger: "hover" });
